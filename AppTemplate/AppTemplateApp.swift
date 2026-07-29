e0426aeb21214519ce260e37139da1b6c323ceb3
John Patrick Echavez <patrickechavez@gmail.com>
2026-07-29 19:55:39 +0800

    Initial Commit

---
+++ AppTemplate/AppTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTemplateApp.swift
+//  AppTemplate
+//
+//  Created by John Patrick Echavez on 7/29/26.
+//
+
+import SwiftUI
+
+@main
+struct AppTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
